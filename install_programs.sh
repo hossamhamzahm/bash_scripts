5f385dae0a2986d9e438f3ef7dbf7450265b0f56
Hossam Hamza <hossamhamza.hm@gmail.com>
2021-10-05 15:59:53 +0200

    add mongo and caffeine

--- install_programs.sh
+++ install_programs.sh
@@ -22,6 +22,10 @@ sudo snap install discord
 sudo snap install zoom-client
 sudo snap install node
 sudo apt install python3
+sudo snap install mailspring
+sudo snap install postman
+sudo snap install slack
+sudo apt install mysql-server
 #sudo apt install vmware
 #sudo apt install teams
 
@@ -35,15 +39,30 @@ sudo apt update
 sudo apt install brave-browser
 
 
-
-
-sudo snap install mailspring
-sudo snap install postman
-sudo snap install slack
-sudo apt install mysql-server
 #mongodb
+sudo apt-get install gnupg
+wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+echo "mongodb-org hold" | sudo dpkg --set-selections
+echo "mongodb-org-database hold" | sudo dpkg --set-selections
+echo "mongodb-org-server hold" | sudo dpkg --set-selections
+echo "mongodb-org-shell hold" | sudo dpkg --set-selections
+echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
+echo "mongodb-org-tools hold" | sudo dpkg --set-selections
+sudo systemctl start mongod
+sudo systemctl daemon-reload
+sudo systemctl status mongod
+sudo systemctl enable mongod
+sudo systemctl stop mongod
+sudo systemctl restart mongod
+# mongosh
+
+
+
 #arduino ide
-#caffeine
+sudo apt-get install caffeine
 #gitkraken
 
 
